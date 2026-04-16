63c2d9d542c56630ab7cf795bced697068a89f86
OmarAyman1234 <omarayman9124@gmail.com>
2026-04-16 13:38:04 +0200

    All geo tranformations, but shearing has a problem

--- GraphicsProj/TransformForm.Designer.cs
+++ GraphicsProj/TransformForm.Designer.cs
@@ -29,11 +29,36 @@
         private void InitializeComponent()
         {
             pictureBox = new PictureBox();
+            xTrSlider = new TrackBar();
+            yTrSlider = new TrackBar();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            yScSlider = new TrackBar();
+            xScSlider = new TrackBar();
+            rotSlider = new TrackBar();
+            label9 = new Label();
+            label5 = new Label();
+            label8 = new Label();
+            yShSlider = new TrackBar();
+            xShSlider = new TrackBar();
+            label10 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)xTrSlider).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)yTrSlider).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)yScSlider).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)xScSlider).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)rotSlider).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)yShSlider).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)xShSlider).BeginInit();
             SuspendLayout();
             // 
             // pictureBox
             // 
+            pictureBox.BorderStyle = BorderStyle.FixedSingle;
             pictureBox.Image = Properties.Resources.fruits;
             pictureBox.Location = new Point(784, 40);
             pictureBox.Name = "pictureBox";
@@ -42,16 +67,198 @@
             pictureBox.TabIndex = 0;
             pictureBox.TabStop = false;
             // 
+            // xTrSlider
+            // 
+            xTrSlider.Location = new Point(23, 92);
+            xTrSlider.Maximum = 150;
+            xTrSlider.Minimum = -150;
+            xTrSlider.Name = "xTrSlider";
+            xTrSlider.Size = new Size(156, 69);
+            xTrSlider.TabIndex = 1;
+            // 
+            // yTrSlider
+            // 
+            yTrSlider.Location = new Point(201, 92);
+            yTrSlider.Maximum = 150;
+            yTrSlider.Minimum = -150;
+            yTrSlider.Name = "yTrSlider";
+            yTrSlider.Size = new Size(156, 69);
+            yTrSlider.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(23, 64);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 25);
+            label1.TabIndex = 3;
+            label1.Text = "X: 0";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(201, 64);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Y: 0";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Impact", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(23, 20);
+            label3.Name = "label3";
+            label3.Size = new Size(146, 35);
+            label3.TabIndex = 5;
+            label3.Text = "Translation";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Impact", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(23, 164);
+            label4.Name = "label4";
+            label4.Size = new Size(101, 35);
+            label4.TabIndex = 6;
+            label4.Text = "Scaling";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(201, 207);
+            label6.Name = "label6";
+            label6.Size = new Size(41, 25);
+            label6.TabIndex = 10;
+            label6.Text = "Y: 0";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(23, 207);
+            label7.Name = "label7";
+            label7.Size = new Size(42, 25);
+            label7.TabIndex = 9;
+            label7.Text = "X: 0";
+            // 
+            // yScSlider
+            // 
+            yScSlider.Location = new Point(201, 235);
+            yScSlider.Maximum = 20;
+            yScSlider.Name = "yScSlider";
+            yScSlider.Size = new Size(156, 69);
+            yScSlider.TabIndex = 8;
+            yScSlider.Value = 10;
+            // 
+            // xScSlider
+            // 
+            xScSlider.Location = new Point(23, 235);
+            xScSlider.Maximum = 20;
+            xScSlider.Name = "xScSlider";
+            xScSlider.Size = new Size(156, 69);
+            xScSlider.TabIndex = 7;
+            xScSlider.Value = 10;
+            // 
+            // rotSlider
+            // 
+            rotSlider.Location = new Point(23, 327);
+            rotSlider.Maximum = 180;
+            rotSlider.Minimum = -180;
+            rotSlider.Name = "rotSlider";
+            rotSlider.Size = new Size(156, 69);
+            rotSlider.TabIndex = 12;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Impact", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(23, 289);
+            label9.Name = "label9";
+            label9.Size = new Size(112, 35);
+            label9.TabIndex = 11;
+            label9.Text = "Rotation";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(201, 429);
+            label5.Name = "label5";
+            label5.Size = new Size(41, 25);
+            label5.TabIndex = 17;
+            label5.Text = "Y: 0";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(23, 429);
+            label8.Name = "label8";
+            label8.Size = new Size(42, 25);
+            label8.TabIndex = 16;
+            label8.Text = "X: 0";
+            // 
+            // yShSlider
+            // 
+            yShSlider.Location = new Point(201, 457);
+            yShSlider.Maximum = 5;
+            yShSlider.Minimum = -5;
+            yShSlider.Name = "yShSlider";
+            yShSlider.Size = new Size(156, 69);
+            yShSlider.TabIndex = 15;
+            // 
+            // xShSlider
+            // 
+            xShSlider.Location = new Point(23, 457);
+            xShSlider.Maximum = 5;
+            xShSlider.Minimum = -5;
+            xShSlider.Name = "xShSlider";
+            xShSlider.Size = new Size(156, 69);
+            xShSlider.TabIndex = 14;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Impact", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.Location = new Point(23, 386);
+            label10.Name = "label10";
+            label10.Size = new Size(119, 35);
+            label10.TabIndex = 13;
+            label10.Text = "Shearing";
+            // 
             // TransformForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1334, 678);
+            Controls.Add(label5);
+            Controls.Add(label8);
+            Controls.Add(yShSlider);
+            Controls.Add(xShSlider);
+            Controls.Add(label10);
+            Controls.Add(rotSlider);
+            Controls.Add(label9);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(yScSlider);
+            Controls.Add(xScSlider);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(yTrSlider);
+            Controls.Add(xTrSlider);
             Controls.Add(pictureBox);
             Name = "TransformForm";
             Text = "TransformForm";
             ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)xTrSlider).EndInit();
+            ((System.ComponentModel.ISupportInitialize)yTrSlider).EndInit();
+            ((System.ComponentModel.ISupportInitialize)yScSlider).EndInit();
+            ((System.ComponentModel.ISupportInitialize)xScSlider).EndInit();
+            ((System.ComponentModel.ISupportInitialize)rotSlider).EndInit();
+            ((System.ComponentModel.ISupportInitialize)yShSlider).EndInit();
+            ((System.ComponentModel.ISupportInitialize)xShSlider).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -59,5 +266,22 @@
         private Panel panel1;
         private PictureBox imgDisplay;
         private PictureBox pictureBox;
+        private TrackBar xTrSlider;
+        private TrackBar yTrSlider;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label6;
+        private Label label7;
+        private TrackBar yScSlider;
+        private TrackBar xScSlider;
+        private TrackBar rotSlider;
+        private Label label9;
+        private Label label5;
+        private Label label8;
+        private TrackBar yShSlider;
+        private TrackBar xShSlider;
+        private Label label10;
     }
 }
--- GraphicsProj/TransformForm.cs
+++ GraphicsProj/TransformForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,190 @@ namespace GraphicsProj
 {
     public partial class TransformForm : Form
     {
+        private Bitmap originalImage;
+
         public TransformForm()
         {
             InitializeComponent();
+
+            if (pictureBox.Image != null)
+            {
+                originalImage = new Bitmap(pictureBox.Image);
+            }
+
+            xTrSlider.Scroll += Sliders_Scroll;
+            yTrSlider.Scroll += Sliders_Scroll;
+
+            xScSlider.Scroll += Sliders_Scroll;
+            yScSlider.Scroll += Sliders_Scroll;
+
+            rotSlider.Scroll += Sliders_Scroll;
+
+            xShSlider.Scroll += Sliders_Scroll;
+            yShSlider.Scroll += Sliders_Scroll;
         }
 
+        private void Sliders_Scroll(object sender, EventArgs e)
+        {
+            ApplyTransformations();
+        }
+
+        private void ApplyTransformations()
+        {
+            if (originalImage == null) return;
+
+            // 1. Get Values
+            // Logic to handle the "Snap" from 0.1 to -1
+            double sx;
+            if (xScSlider.Value <= 0)
+            {
+                sx = -1.0; // Force reflection if slider goes below 0.1 equivalent
+            }
+            else
+            {
+                sx = xScSlider.Value / 10.0; // Standard 0.1 to 2.0 range
+            }
+
+            double sy;
+            if (yScSlider.Value <= 0)
+            {
+                sy = -1.0;
+            }
+            else
+            {
+                sy = yScSlider.Value / 10.0;
+            }
+            double rad = rotSlider.Value * Math.PI / 180.0;
+            double shx = -xShSlider.Value / 10.0;
+            double shy = -yShSlider.Value / 10.0;
+            int dx = xTrSlider.Value;
+            int dy = yTrSlider.Value;
+
+            double cx = originalImage.Width / 2.0;
+            double cy = originalImage.Height / 2.0;
+
+            // 2. Center transformation (move to origin)
+            double[,] toCenter = { { 1, 0, -cx }, { 0, 1, -cy }, { 0, 0, 1 } };
+
+            // 3. Transformations around center
+            double[,] scale = { { sx, 0, 0 }, { 0, sy, 0 }, { 0, 0, 1 } };
+            double[,] rot = { { Math.Cos(rad), -Math.Sin(rad), 0 }, { Math.Sin(rad), Math.Cos(rad), 0 }, { 0, 0, 1 } };
+            double[,] shear = { { 1, shx, 0 }, { shy, 1, 0 }, { 0, 0, 1 } };
+
+            // 4. Move back from center with translation
+            double[,] fromCenter = { { 1, 0, cx + dx }, { 0, 1, cy + dy }, { 0, 0, 1 } };
+
+            // 5. Combine in correct order: fromCenter * shear * rot * scale * toCenter
+            double[,] combined = MultiplyMatrices(fromCenter, shear);
+            combined = MultiplyMatrices(combined, rot);
+            combined = MultiplyMatrices(combined, scale);
+            combined = MultiplyMatrices(combined, toCenter);
+
+            // 6. Apply
+            Bitmap transformedImage = ApplyMatrix(originalImage, combined);
+
+            if (pictureBox.Image != null && pictureBox.Image != originalImage)
+                pictureBox.Image.Dispose();
+
+            pictureBox.Image = transformedImage;
+        }
+
+        private double[,] MultiplyMatrices(double[,] A, double[,] B)
+        {
+            double[,] result = new double[3, 3];
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    for (int k = 0; k < 3; k++)
+                        result[i, j] += A[i, k] * B[k, j];
+            return result;
+        }
+
+        private Bitmap ApplyMatrix(Bitmap source, double[,] matrix)
+        {
+            int width = source.Width;
+            int height = source.Height;
+            Bitmap result = new Bitmap(width, height, source.PixelFormat);
+
+            double[,] inverse = InvertMatrix(matrix);
+
+            var rect = new Rectangle(0, 0, width, height);
+            BitmapData srcData = source.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            BitmapData dstData = result.LockBits(rect, ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+
+            int bytesPerPixel = 4;
+            int stride = srcData.Stride;
+
+            unsafe
+            {
+                byte* src = (byte*)srcData.Scan0;
+                byte* dst = (byte*)dstData.Scan0;
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        // Inverse matrix handles everything — no manual center shift
+                        double srcX = inverse[0, 0] * x + inverse[0, 1] * y + inverse[0, 2];
+                        double srcY = inverse[1, 0] * x + inverse[1, 1] * y + inverse[1, 2];
+
+                        int dstIdx = y * stride + x * bytesPerPixel;
+                        int roundedX = (int)Math.Round(srcX);
+                        int roundedY = (int)Math.Round(srcY);
+
+                        if (roundedX >= 0 && roundedX < width && roundedY >= 0 && roundedY < height)
+                        {
+                            int srcIdx = roundedY * stride + roundedX * bytesPerPixel;
+                            dst[dstIdx + 0] = src[srcIdx + 0];
+                            dst[dstIdx + 1] = src[srcIdx + 1];
+                            dst[dstIdx + 2] = src[srcIdx + 2];
+                            dst[dstIdx + 3] = src[srcIdx + 3];
+                        }
+                        else
+                        {
+                            dst[dstIdx + 0] = 255;
+                            dst[dstIdx + 1] = 255;
+                            dst[dstIdx + 2] = 255;
+                            dst[dstIdx + 3] = 255;
+                        }
+                    }
+                }
+            }
+
+            source.UnlockBits(srcData);
+            result.UnlockBits(dstData);
+
+            return result;
+        }
+
+        private double[,] InvertMatrix(double[,] m)
+        {
+            // For a 3x3 affine matrix, compute the inverse using determinant
+            double det = m[0, 0] * (m[1, 1] * m[2, 2] - m[1, 2] * m[2, 1])
+                       - m[0, 1] * (m[1, 0] * m[2, 2] - m[1, 2] * m[2, 0])
+                       + m[0, 2] * (m[1, 0] * m[2, 1] - m[1, 1] * m[2, 0]);
+
+            if (Math.Abs(det) < 1e-10) return m; // Singular, return as-is
+
+            double invDet = 1.0 / det;
+
+            return new double[,]
+            {
+        {
+            (m[1,1]*m[2,2] - m[1,2]*m[2,1]) * invDet,
+            (m[0,2]*m[2,1] - m[0,1]*m[2,2]) * invDet,
+            (m[0,1]*m[1,2] - m[0,2]*m[1,1]) * invDet
+        },
+        {
+            (m[1,2]*m[2,0] - m[1,0]*m[2,2]) * invDet,
+            (m[0,0]*m[2,2] - m[0,2]*m[2,0]) * invDet,
+            (m[0,2]*m[1,0] - m[0,0]*m[1,2]) * invDet
+        },
+        {
+            (m[1,0]*m[2,1] - m[1,1]*m[2,0]) * invDet,
+            (m[0,1]*m[2,0] - m[0,0]*m[2,1]) * invDet,
+            (m[0,0]*m[1,1] - m[0,1]*m[1,0]) * invDet
+        }
+            };
+        }
     }
 }
